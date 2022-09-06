5e40d5cf37fdc36803b7ce9150fd6ec4d81b2031
Sayantan Majee <sayantan.majee24@gmail.com>
2022-09-06 12:27:41 +0530

    Amazon clone starter templete commit

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Amazon 2.0</title>
+      </Head>
+      <center className="grid place-items-center mt-10 text-3xl">
+          <h1 className="font-extrabold pb-10">This is Amazon 2.0 Starter templete</h1>
+          <h2>Tech used: React | Redux | Tailwind | Firebse</h2>
+      </center>
+    </div>
+  );
+}
